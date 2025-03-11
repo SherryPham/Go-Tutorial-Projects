984df9b1465b22ac2f534458c09cbf8bdabda8e5
SherryPham <phamthuthu204@gmail.com>
2025-03-11 23:45:32 +1100

    outline functions

---
+++ secure-login-portal/main.go
@@ -0,0 +1,32 @@
+pakage main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Login struct {
+	HashedPassword string
+	Session string
+	CRSFToken string
+}
+
+// Key is the username
+var users = map[string]Login{}
+
+func main() {
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/protected", protected)
+	http.ListenAndServe(":8080", nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {}
+
+func login(w http.ResponseWriter, r *http.Request) {}
+
+func logout(w http.ResponseWriter, r *http.Request) {}
+
+func protected(w http.ResponseWriter, r *http.Request) {}
